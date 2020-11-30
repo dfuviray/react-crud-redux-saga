a9e655f0b00c44165960f8a003c7b22c4748fc32
Deo <deo.uviray@gmail.com>
2020-11-30 21:28:34 +0800

    Add Header component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
+import Header from "./components/header/Header";
+import "font-awesome/css/font-awesome.min.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/header/Header.js
@@ -0,0 +1,14 @@
+import "./header.css";
+
+const Header = () => {
+  return (
+    <div className="Header">
+      <p>Locations</p>
+      <button>
+        <i className="fa fa-plus" />
+      </button>
+    </div>
+  );
+};
+
+export default Header;
